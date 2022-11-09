1ef74922152d72ca097ed36f1833795cda227a5d
unknown <nguyentrungdung12a@gmail.com>
2022-11-09 21:44:10 +0700

    init

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
-
+import { combineReducers } from '@reduxjs/toolkit';
 export const store = configureStore({
-  reducer: {
+  
+  reducer:combineReducers({
     counter: counterReducer,
-  },
+  },) 
 });
 
 export type AppDispatch = typeof store.dispatch;
